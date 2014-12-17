aa6279a1b990172c9528c9c3f23e0711525024e5
Austin L. Chang <austinlchang@gmail.com>
2014-12-16 23:06:29 -0800

    add length restrict to user_info

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -2,4 +2,13 @@ class UserInfo < ActiveRecord::Base
 
   belongs_to :user
 
+  validates :first_name, length: { maximum: 100 }
+  validates :last_name, length: { maximum: 100 }
+  validates :major, length: { maximum: 100 }
+  validates :website_url, length: { maximum: 100 }
+  validates :github_url, length: { maximum: 100 }
+  validates :linkedin_url, length: { maximum: 100 }
+  validates :coolest_built, length: { maximum: 200 }
+  validates :previous_hackathons, length: { maximum: 200 }
+
 end
