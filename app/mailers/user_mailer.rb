2b9e5910e3d1d5bf7fe1585dfe108071ca4a438e
Austin L. Chang <austinlchang@gmail.com>
2014-12-26 04:39:11 -0800

    add forgot password mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,20 @@
 class UserMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers
 
-  @@template = "hp-emailconfirm|body"
+  @@template = { confirm: "hp-emailconfirm|body", password: "hp-passwordreset|body" }
 
   def confirmation_instructions(record, token, opts={})
-    headers["X-MC-Template"] = @@template
-    opts[:subject] = "Account Confirmation Instructions | Hackpoly 2015"
+    headers["X-MC-Template"] = @@template[:confirm]
+    opts[:subject] = "Hackpoly Account Confirmation"
     @token = token
     devise_mail(record, :confirmation_instructions, opts)
   end
 
   def reset_password_instructions(record, token, opts={})
+    headers["X-MC-Template"] = @@template[:password]
+    opts[:subject] = "Hackpoly Password Reset"
+    @token = token
+    devise_mail(record, :reset_password_instructions, opts)
   end
 
   def unlock_instructions(record, token, opts={})
