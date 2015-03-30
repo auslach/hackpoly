665f3e96ddb6fcbd622252c67f4894e0eb282058
Austin L. Chang <austinlchang@gmail.com>
2015-03-30 11:36:12 -0700

    expire hsts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :expire_hsts
 
   # Devise
   def after_sign_in_path_for(resource)
@@ -11,6 +12,12 @@ class ApplicationController < ActionController::Base
     else
       user_application_path
     end
+  end
+
+private
+
+  def expire_hsts
+    response.headers["Strict-Transport-Security"] = 'max-age=0'
   end
 
 end
